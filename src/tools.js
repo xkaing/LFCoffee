21a1b8d8c585465821f4e2360e65eb7515b73281
wxk <wxk.cn@icloud.com>
2024-02-24 23:46:34 +0800

    feat:替换名字改为新增名字字段，不修改原始数据

--- src/tools.js
+++ src/tools.js
@@ -1,4 +1,4 @@
-const dictMarkName = {
+const mapKeyName = {
     "android-1": "姜振",
     "android-2": "刘磊",
     "android-3": "李泽晋",
@@ -17,16 +17,16 @@ const dictMarkName = {
       // 处理数组
       item.drinker_list = item.drinker_list.map(mapPayerInObject); // 递归遍历数组
       if ("payer" in item) {
-        item.payer = dictMarkName[item.payer] || item.payer;
+        item.payer = mapKeyName[item.payer] || item.payer;
       } else if ("drinker" in item) {
-        item.drinker = dictMarkName[item.drinker] || item.drinker;
+        item.drinker = mapKeyName[item.drinker] || item.drinker;
       }
     } else if (typeof item === "object" && item !== null) {
       // 处理非数组
       if ("payer" in item) {
-        item.payer = dictMarkName[item.payer] || item.payer;
+        item.payer = mapKeyName[item.payer] || item.payer;
       } else if ("drinker" in item) {
-        item.drinker = dictMarkName[item.drinker] || item.drinker;
+        item.drinker = mapKeyName[item.drinker] || item.drinker;
       }
     }
     return item;
@@ -34,5 +34,34 @@ const dictMarkName = {
   
   export const markBecomeName = (sData) => {
     let nData = sData.map(mapPayerInObject);
+    return nData;
+  };
+
+  const addRealNameInObject = (item) => {
+    if (
+      typeof item === "object" &&
+      item !== null &&
+      Array.isArray(item.drinker_list)
+    ) {
+      // 处理数组
+      item.drinker_list = item.drinker_list.map(addRealNameInObject); // 递归遍历数组
+      if ("payer" in item) {
+        item.payer_name = mapKeyName[item.payer] || item.payer;
+      } else if ("drinker" in item) {
+        item.drinker_name = mapKeyName[item.drinker] || item.drinker;
+      }
+    } else if (typeof item === "object" && item !== null) {
+      // 处理非数组
+      if ("payer" in item) {
+        item.payer_name = mapKeyName[item.payer] || item.payer;
+      } else if ("drinker" in item) {
+        item.drinker_name = mapKeyName[item.drinker] || item.drinker;
+      }
+    }
+    return item;
+  };
+  
+  export const addRealName = (sData) => {
+    let nData = sData.map(addRealNameInObject);
     return nData;
   };
