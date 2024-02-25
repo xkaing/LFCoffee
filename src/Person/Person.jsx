742306171da60a6798f96ddf32aae2c7bf07d81d
wxk <wxk.cn@icloud.com>
2024-02-25 17:40:03 +0800

    feat:更新图片资源

--- src/Person/Person.jsx
+++ src/Person/Person.jsx
@@ -1,14 +1,75 @@
 import { useContext } from "react";
 import { CoffeeDataContext } from "../contexts/CoffeeDataContext.jsx";
+import React from "react";
+import { UserOutlined } from "@ant-design/icons";
+import { Avatar, Flex, Segmented } from "antd";
 
 const Person = () => {
   const coffeeData = useContext(CoffeeDataContext);
   console.log(coffeeData);
   return (
     <div>
-      <h1>Person</h1>
+      <PersonSegmented />
     </div>
   );
 };
 
+const PersonSegmented = () => (
+  <Flex gap="small" align="flex-start" vertical>
+    <Segmented
+      options={[
+        {
+          label: (
+            <div
+              style={{
+                padding: 4,
+              }}
+            >
+              <Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=8" />
+              <div>User 1</div>
+            </div>
+          ),
+          value: "user1",
+        },
+        {
+          label: (
+            <div
+              style={{
+                padding: 4,
+              }}
+            >
+              <Avatar
+                style={{
+                  backgroundColor: "#f56a00",
+                }}
+              >
+                K
+              </Avatar>
+              <div>User 2</div>
+            </div>
+          ),
+          value: "user2",
+        },
+        {
+          label: (
+            <div
+              style={{
+                padding: 4,
+              }}
+            >
+              <Avatar
+                style={{
+                  backgroundColor: "#87d068",
+                }}
+                icon={<UserOutlined />}
+              />
+              <div>User 3</div>
+            </div>
+          ),
+          value: "user3",
+        },
+      ]}
+    />
+  </Flex>
+);
 export default Person;
