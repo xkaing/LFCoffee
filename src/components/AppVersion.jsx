084ae39db65c3b2ec21c65b047adadcf96c446ea
wxk <wxk.cn@icloud.com>
2024-03-04 21:59:00 +0800

    feat：优化买单排期

--- src/components/AppVersion.jsx
+++ src/components/AppVersion.jsx
@@ -3,7 +3,7 @@ import { CodeOutlined } from "@ant-design/icons";
 import DeployTimeline from "./DeployTimeline";
 const { Text } = Typography;
 
-const appVerNum = "v0.11.3-beta";
+const appVerNum = "v0.11.4-beta";
 
 const AppVersion = () => {
   return (
--- src/components/TotalExpendWaiting.jsx
+++ src/components/TotalExpendWaiting.jsx
@@ -1,18 +1,54 @@
 import React, { useContext } from "react";
-import { Steps } from "antd";
-import { TotalInfoContext } from "../contexts/CoffeeDataContext";
+import { Steps, Typography, Space } from "antd";
+import {
+  TotalInfoContext,
+  DateDataContext,
+} from "../contexts/CoffeeDataContext";
 import { addNameInSteps } from "../tools";
+const { Text } = Typography;
 
 const TotalExpendWaiting = () => {
   const totalWaiting = useContext(TotalInfoContext).totalWaiting || [];
+  const dateData = useContext(DateDataContext) || {};
 
-  const allPersonArr = addNameInSteps(totalWaiting[1]); // 排期模版（所有成员）
+  // 排期模版（所有成员）
+  const allPersonArr = addNameInSteps(totalWaiting[1]);
 
-  const lastWaiting = totalWaiting.at(-1); // 当前排期
+  // 当前排期已经买单的
+  const lastWaiting = totalWaiting.at(-1);
+  // 当前排期位置
   const currentIndex = allPersonArr.findIndex(
     (person) => person.payer === lastWaiting.at(-1)
-  ); // 当前排期位置
+  );
+  // 买单的详情
+  const lastWaitingFullInfo = dateData.slice(-lastWaiting.length);
 
-  return <Steps current={currentIndex + 1} items={allPersonArr} />;
+  const allPersonArr2 = allPersonArr.map((person) => {
+    const payerInfo = lastWaitingFullInfo.find(
+      (info) => info.payer === person.payer
+    );
+    if (payerInfo) {
+      return {
+        payer: payerInfo.payer,
+        title: payerInfo.name,
+        description: <StepsDesc payerInfo={payerInfo} />,
+      };
+    } else {
+      return person;
+    }
+  });
+
+  return <Steps current={currentIndex + 1} items={allPersonArr2} />;
 };
+
 export default TotalExpendWaiting;
+
+const StepsDesc = ({ payerInfo }) => {
+  return (
+    <Space direction="vertical" size={"small"}>
+      <Text>日期：{payerInfo.date}</Text>
+      <Text>杯数：{payerInfo.cups}</Text>
+      <Text>均价：¥{payerInfo.average}</Text>
+    </Space>
+  );
+};
