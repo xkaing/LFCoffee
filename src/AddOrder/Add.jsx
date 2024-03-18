305e317d75ffc221aa265896324da35e22c45a58
wxk <wxk.cn@icloud.com>
2024-03-18 20:42:19 +0800

    feat:更新Add组件

--- src/AddOrder/Add.jsx
+++ src/AddOrder/Add.jsx
@@ -1,81 +1,101 @@
 import React from "react";
-import { Button, DatePicker, Form, Input } from "antd";
+import { Button, DatePicker, Form, Input, Typography, Space } from "antd";
 const onFinish = (values) => {
   console.log("Success:", values);
 };
 const onFinishFailed = (errorInfo) => {
   console.log("Failed:", errorInfo);
 };
-const App = () => (
-  <Form
-    name="basic"
-    labelCol={{
-      span: 8,
-    }}
-    wrapperCol={{
-      span: 16,
-    }}
-    style={{
-      maxWidth: 600,
-    }}
-    initialValues={{
-      remember: true,
-    }}
-    onFinish={onFinish}
-    onFinishFailed={onFinishFailed}
-    autoComplete="off"
-    variant="filled"
-  >
-    <Form.Item
-      label="日期"
-      name="date"
-      rules={[
-        {
-          required: true,
-          message: "必须有购买日期!",
-        },
-      ]}
-    >
-      <DatePicker />
-    </Form.Item>
-    <Form.Item
-      label="买单"
-      name="payer"
-      rules={[
-        {
-          required: true,
-          message: "必须有买单人!",
-        },
-      ]}
-    >
-      <Input />
-    </Form.Item>
-    <Form.Item label="收入" name="income">
-      <Input />
-    </Form.Item>
-    <Form.Item
-      label="支出"
-      name="expend"
-      rules={[
-        {
-          required: true,
-          message: "必须有购买咖啡支出!",
-        },
-      ]}
-    >
-      <Input />
-    </Form.Item>
 
-    <Form.Item
+const Add = () => {
+  const [form] = Form.useForm();
+  const onReset = () => {
+    form.resetFields();
+  };
+  return (
+    <Form
+      name="basic"
+      form={form}
+      labelCol={{
+        span: 8,
+      }}
       wrapperCol={{
-        offset: 8,
         span: 16,
       }}
+      style={{
+        maxWidth: 600,
+      }}
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+      variant="filled"
     >
-      <Button type="primary" htmlType="submit">
-        提交订单
-      </Button>
-    </Form.Item>
-  </Form>
-);
-export default App;
+      <Form.Item
+        label="日期"
+        name="date"
+        rules={[
+          {
+            required: true,
+            message: "必须有购买日期!",
+          },
+        ]}
+      >
+        <DatePicker />
+      </Form.Item>
+      <Form.Item
+        label="买单"
+        name="payer"
+        rules={[
+          {
+            required: true,
+            message: "必须有买单人!",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item label="收入" name="income">
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="支出"
+        name="expend"
+        rules={[
+          {
+            required: true,
+            message: "必须有购买咖啡支出!",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Space>
+          <Button type="primary" htmlType="submit">
+            提交订单
+          </Button>
+          <Button htmlType="button" onClick={onReset}>
+            重置
+          </Button>
+        </Space>
+      </Form.Item>
+      <Form.Item noStyle shouldUpdate>
+        {() => (
+          <Typography>
+            <pre>{JSON.stringify(form.getFieldsValue(), null, 2)}</pre>
+          </Typography>
+        )}
+      </Form.Item>
+    </Form>
+  );
+};
+export default Add;
