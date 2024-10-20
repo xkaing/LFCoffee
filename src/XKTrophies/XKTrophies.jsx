c79c7da6fab5d56aa432fa249a259e8a33021925
XiaoKai <wxk.cn@icloud.com>
2024-10-20 19:04:05 +0800

    更新 XKTrophies.jsx

--- src/XKTrophies/XKTrophies.jsx
+++ src/XKTrophies/XKTrophies.jsx
@@ -24,28 +24,48 @@ const XKTrophies = () => {
       title: "ID",
       dataIndex: "key",
       key: "key",
+      width: 50,
     },
     {
       title: "Buy Date",
       dataIndex: "buy_date",
       key: "buy_date",
       render: (buy_date) => <YearColorTag date={buy_date} />,
+      sorter: (a, b) => new Date(a.buy_date) - new Date(b.buy_date),
     },
     {
       title: "Name",
       dataIndex: "game_name",
       key: "game_name",
+      ellipsis: true,
     },
     {
       title: "Source",
       dataIndex: "buy_source",
       key: "buy_source",
       render: (buy_source) => <SourceColorTag channel={buy_source} />,
+      filters: [
+        {
+          text: "Physical Disc",
+          value: "Physical Disc",
+        },
+        {
+          text: "Game Catalogue",
+          value: "Game Catalogue",
+        },
+        {
+          text: "PS Store",
+          value: "PS Store",
+        }
+      ],
+      onFilter: (value, record) => record.buy_source.indexOf(value) === 0,
     },
     {
-      title: "Price(¥)",
+      title: "Price",
       key: "buy_price",
       dataIndex: "buy_price",
+      width: 70,
+      sorter: (a, b) => a.buy_price - b.buy_price,
     },
     {
       title: "Platinum Date",
@@ -54,52 +74,115 @@ const XKTrophies = () => {
       render: (play_platinum_date) => (
         <YearColorTag date={play_platinum_date} />
       ),
+      sorter: (a, b) => {
+        const defaultDate = new Date();
+        const aTime = a.play_platinum_date === "-" ? defaultDate : new Date(a.play_platinum_date);
+        const bTime = b.play_platinum_date === "-" ? defaultDate : new Date(b.play_platinum_date);
+        return aTime - bTime;
+      }
     },
     {
       title: "Trophy",
       key: "game_trophy",
       dataIndex: "game_trophy",
       render: (game_trophy) => <TrophyProgress progress={game_trophy} />,
+      width: 70,
+      sorter: (a, b) => a.game_trophy - b.game_trophy,
     },
     {
       title: "Developer",
       key: "game_developer",
       dataIndex: "game_developer",
+      ellipsis: true,
     },
     {
       title: "Publisher",
       key: "game_publisher",
       dataIndex: "game_publisher",
+      ellipsis: true,
     },
     {
       title: "Release Date",
       key: "game_release_date",
       dataIndex: "game_release_date",
       render: (game_release_date) => <YearColorTag date={game_release_date} />,
+      sorter: (a, b) => new Date(a.game_release_date) - new Date(b.game_release_date),
     },
     {
       title: "time(h)",
       key: "play_time",
       dataIndex: "play_time",
+      sorter: (a, b) => a.play_time - b.play_time,
     },
   ];
+
+  const dlcColumns = [
+    {
+      title: "",
+      dataIndex: "key",
+      width: 100
+    },
+    {
+      title: "Name",
+      dataIndex: "game_name",
+      width: 100,
+      ellipsis: true,
+    },
+    {
+      title: "Release Date",
+      dataIndex: "game_release_date",
+      render: (game_release_date) => <YearColorTag date={game_release_date} />,
+      width: 100
+    },
+    {
+      title: "Buy Date",
+      dataIndex: "buy_date",
+      render: (buy_date) => <YearColorTag date={buy_date} />,
+      width: 100
+    },
+
+    {
+      title: "Source",
+      dataIndex: "buy_source",
+      render: (buy_source) => <SourceColorTag channel={buy_source} />,
+      width: 100
+    },
+    {
+      title: "Price",
+      dataIndex: "buy_price",
+      width: 50
+    },
+    {
+      title: "Trophy",
+      dataIndex: "game_trophy",
+      render: (game_trophy) => <TrophyProgress progress={game_trophy} />,
+    },
+  ]
+
+  const dlcTable = (record) => {
+    const dlcArrData = record.game_dlc.map((item, index) => {
+      return { ...item, key: `dlc ${index + 1}` };
+    });
+    return (<Table columns={dlcColumns} dataSource={dlcArrData} pagination={false} style={{
+      marginLeft: 155,
+    }} />)
+  }
+
   return (
     <Table
       columns={columns}
       dataSource={reactArrData}
       size="small"
       pagination={{
-        pageSize: 60,
+        pageSize: 100,
       }}
-      footer={() => (
-        <Text strong>
-          PSN为58款，Call of Duty: Modern Warfare 3为DLC，Final Fantasy VII
-          Rebirth还未发售
-        </Text>
-      )}
-      // scroll={{
-      //   y: 600,
-      // }}
+      scroll={{
+        y: 55 * 15,
+      }}
+      expandable={{
+        expandedRowRender: (record) => dlcTable(record),
+        rowExpandable: (record) => record.game_dlc,
+      }}
     />
   );
 };
